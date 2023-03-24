fdf8b1113d5550e3596dd3ca1545c70ac90912e8
Tiltek <tongisttonji@gmail.com>
2023-03-24 15:29:59 +0300

    Latest update on March 15 2023

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const {v4: uuidV4} = require('uuid')
+
+
+const PORT = 3000
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+
+app.get('/', (req, res)=> {
+    // creates new room
+    res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:roomId', (req,res)=> {
+    res.render('room', {roomId: req.params.roomId})
+})
+
+
+
+
+server.listen(PORT, () => {
+    console.log(`Server running at ${PORT}`)
+})
